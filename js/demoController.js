a064024e561208c0994b7eb560de7cc629343a9e
creativelycommon <thomas@creativelycommon.co.uk>
2016-03-09 11:14:24 +0000

    File Input
    Adding in file selector for audio element source.
    
    Audio element currently defaults to an example live stream

--- js/demoController.js
+++ js/demoController.js
@@ -12,7 +12,19 @@ window.onload = function() {
 	streamSource.connect(nodeToAnalyse);
 	nodeToAnalyse.connect(audioContext.destination);
 
+
+	//create input for local files
+	var fileSelector = document.createElement("input");
+	fileSelector.type = 'file';
+	fileSelector.onchange = function(e){
+    	var file = e.currentTarget.files[0];
+    	var objectUrl = URL.createObjectURL(file);
+    	audioElement.src= objectUrl;
+	};
+
+
 	document.getElementById('lufsContainer').appendChild(audioElement);
+	document.getElementById('lufsContainer').appendChild(fileSelector);
 
 	var lufsControllerOne = new lufsController(
 		{
